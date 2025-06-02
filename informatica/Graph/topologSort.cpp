7f9d8af84d28fe99f7af53418702fde7aa6fc693
AndreMaestro <rybalovandrey2007@gmail.com>
2025-06-02 22:17:47 +0400

    adding functions: Gr, main

---
+++ informatica/Graph/topologSort.cpp
@@ -0,0 +1,42 @@
+/*Дан ориентированный граф. Подсчитать полустепень исхода каждой вершины.*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+vector<vector<int>> Gr(int n, int m){
+    vector<vector<int>> Gr;
+    Gr.resize(n);
+
+    for(int i = 0; i < m; i++){
+        int x, y;
+        cin >> x;
+        cin >> y;
+
+        if (x > n || y > n || x < 0 || y < 0) continue;
+
+        Gr[x].push_back(y);
+    }
+
+    for (int i = 0; i < n; i++){
+        sort(Gr[i].begin(), Gr[i].end());
+        (Gr[i].erase(unique(Gr[i].begin(), Gr[i].end()), Gr[i].end()));
+    }
+
+    return Gr;
+}
+
+
+int main(){
+    int n, m;
+
+    cout << "Enter the number of vertices (n): "; cin >> n;
+    cout << "Enter the number of edges (m): "; cin >> m;
+    
+    vector<vector<int>> gr = Gr(n, m);
+
+       
+    return 0;
+}
