71faef2e254dafdf32e39b13ab8b2cbcc4a64d39
AndreMaestro <rybalovandrey2007@gmail.com>
2025-06-02 22:51:49 +0400

    adding functions: dfs, TopologicalSort

--- informatica/Graph/topologSort.cpp
+++ informatica/Graph/topologSort.cpp
@@ -1,8 +1,9 @@
-/*Дан ориентированный граф. Подсчитать полустепень исхода каждой вершины.*/
+/*Дан ориентированный граф. Если это возможно, реализовать топологическую сортировку*/
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
 
 
@@ -28,6 +29,48 @@ vector<vector<int>> Gr(int n, int m){
     return Gr;
 }
 
+bool dfs(const vector<vector<int>> &gr, vector<bool> &visited, vector<bool> &inStack, int v, stack<int> &st){
+    visited[v] = true;
+    inStack[v] = true;
+
+    for(int u : gr[v]){
+        if (!visited[u]) {
+            if(!dfs(gr, visited, inStack, u, st))
+                return false;
+        }
+        else if (inStack[u])
+            return false;
+    }
+
+    inStack[v] = false;
+    st.push(v);
+    return true;
+}
+
+vector<int> TopologicalSort (const vector<vector<int>> gr){
+    int n = gr.size();
+    vector<bool> inStack(n, false);
+    vector<bool> visited(n, false);
+    stack<int> st;
+    bool ExistCycle = false;
+
+    for(int i = 0; i < n; i++){
+        if (!visited[i]){
+            if (!dfs(gr, visited, inStack, i, st)){
+                ExistCycle = true;
+                return{};
+            }
+        }
+    }
+
+    vector<int> res;
+    while(!st.empty()){
+        res.push_back(st.top());
+        st.pop();
+    }
+
+    return res;
+}
 
 int main(){
     int n, m;
@@ -36,7 +79,11 @@ int main(){
     cout << "Enter the number of edges (m): "; cin >> m;
     
     vector<vector<int>> gr = Gr(n, m);
+    vector<int> sort = TopologicalSort(gr);
+    
+    for(int v : sort){
+        cout << v << " ";
+    }
 
-       
     return 0;
 }
